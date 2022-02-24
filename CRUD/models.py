c4eb80f4613e8f44132c056fbe288ee57488dba3
Ramon_Parra <rparracandia@gmail.com>
2022-02-24 16:42:34 -0300

    added models

--- CRUD/models.py
+++ CRUD/models.py
@@ -1,3 +1,18 @@
+import uuid
 from django.db import models
 
+
 # Create your models here.
+
+class User(models.Model):
+
+    id = models.UUIDField(
+        primary_key=True, 
+        default=uuid.uuid4,
+        editable=False, 
+        unique=True) 
+
+    name = models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100)
+    email=models.EmailField(unique=True)
+    birthday=models.DateField()
