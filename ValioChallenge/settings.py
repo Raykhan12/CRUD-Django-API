386bb2bab341408474462d8028852139f1b25319
Ramon_Parra <rparracandia@gmail.com>
2022-02-24 16:45:57 -0300

    updated setings

--- ValioChallenge/settings.py
+++ ValioChallenge/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',    
+    'rest_framework',
+    'CRUD'
 ]
 
 MIDDLEWARE = [
@@ -110,6 +112,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+DATE_INPUT_FORMATS = ['%Y-%m-%d']
 
 
 # Static files (CSS, JavaScript, Images)
